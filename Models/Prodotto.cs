96e6ea391b8d219ceea499d9c07221966900031d
Ginevra9684 <ginevravallebona98@gmail.com>
2024-10-25 14:25:25 +0200

    Aggiunti modelli Prodotto e Categoria

---
+++ Models/Prodotto.cs
@@ -0,0 +1,11 @@
+public class Prodotto
+{
+    public int Id { get; set;}
+    public string Nome { get; set; }
+
+    public decimal Prezzo { get; set; }
+
+    public int Quantita { get; set; }
+
+    public int Id_categoria { get; set; }
+}
