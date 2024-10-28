2439c4221ea3cd9e00ec8b1e6b583fcc3ea6e484
Neomatrix0 <danimusci@yahoo.it>
2024-10-28 16:22:47 +0100

    Aggiunta ClienteView

---
+++ Views/ClienteView.cs
@@ -0,0 +1,38 @@
+public class ClienteView : BaseView
+{
+    // Metodo per visualizzare il menu relativo ai clienti
+    public void ShowClientMenu()
+    {
+        Stampa(" 14 Aggiungere un nuovo cliente");
+        Stampa(" 15 Visualizzare tutti i clienti");
+        
+      //  Stampa(" 16 Modificare un cliente esistente");
+      //  Stampa(" - Eliminare un cliente");
+        
+    }
+
+
+ // Metodo che riceve una lista di clienti e li visualizza
+    public void VisualizzaClienti(List<Cliente> clienti)
+    {
+        Stampa("Elenco Clienti:");
+        foreach (var cliente in clienti)
+        {
+            Stampa($"ID: {cliente.id}, Nome: {cliente.Nome}");
+        }
+    }
+
+
+
+
+     // Metodo per aggiungere un nuovo cliente con il nome
+    public string InserisciCliente()
+    {
+        Stampa("Inserisci il nome del cliente:");
+        return GetInput();
+
+      
+    }
+
+    
+}
