c5d797da79eeaabc97b9ce5f71f0332cb766c1fc
Neomatrix0 <danimusci@yahoo.it>
2024-10-28 07:13:48 +0100

    modifica Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@
 {
     static void Main(string[] args)
     {
-        Model model = new Model();
-        ProductView productView = new ProductView();   // Creazione di un'istanza della vista dei prodotti (ProductView) 
-        CategoryView categoryView = new CategoryView(); // Creazione di un'istanza della vista per le categorie (CategoryView)
-        Controller controller = new Controller(model, productView, categoryView);
+        // Creazione delle istanze del Model e delle Views
+        var model = new Model();
+        var productView = new ProductView();   // Istanza della vista dei prodotti
+        var categoryView = new CategoryView(); // Istanza della vista delle categorie
+
+        // Creazione del Controller, passando le dipendenze al costruttore
+        var controller = new Controller(model, productView, categoryView);
+
+        // Avvio del menu principale
         controller.MainMenu();
     }
 }
