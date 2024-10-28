42a2769833205ac1aed2ba0158cf55882e412936
Ginevra9684 <ginevravallebona98@gmail.com>
2024-10-28 14:06:47 +0100

    solution cancellata

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,6 +1,6 @@
 public class Cliente : Utente
 {
-    public int CodiceCliente{get; set}
+    public int CodiceCliente{get; set;}
     public string Azienda { get; set; }
     public string Indirizzo { get; set; }
     
