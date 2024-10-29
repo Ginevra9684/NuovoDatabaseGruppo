29ed04b5bcb6d63924976c6d4bac08fdf0e9ac45
Serghej <ivaldi500@gmail.com>
2024-10-29 09:47:22 +0100

    Fix classe Ordini

--- Models/Ordini.cs
+++ Models/Ordini.cs
@@ -1,7 +1,5 @@
 public class Ordini
 {
     public int Id { get; set; }
-    public DateTime DataAcquisto { get; set; }
-    public decimal TotImport { get; set; } // Calcolerà il totale dell'ordine (TotImport = Prodotto * Qty)
-   
+    public DateTime DataAcquisto { get; set; }   
 }
