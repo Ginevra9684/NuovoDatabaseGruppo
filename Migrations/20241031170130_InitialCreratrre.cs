cb82a97d144b86872809957809088869c108f323
Pozzame <pozzame@gmail.com>
2024-10-31 18:02:29 +0100

    Ok

---
+++ Migrations/20241031170130_InitialCreratrre.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NuovoDatabaseGruppo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreratrre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorie", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clienti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clienti", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prodotti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Prezzo = table.Column<double>(type: "REAL", nullable: false),
+                    Giacenza = table.Column<int>(type: "INTEGER", nullable: false),
+                    Id_categoria = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prodotti", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Prodotti_Categorie_Id_categoria",
+                        column: x => x.Id_categoria,
+                        principalTable: "Categorie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ordini",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    DataAcquisto = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Quantita = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ProdottoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordini", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ordini_Clienti_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clienti",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Ordini_Prodotti_ProdottoId",
+                        column: x => x.ProdottoId,
+                        principalTable: "Prodotti",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordini_ClienteId",
+                table: "Ordini",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordini_ProdottoId",
+                table: "Ordini",
+                column: "ProdottoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prodotti_Id_categoria",
+                table: "Prodotti",
+                column: "Id_categoria");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ordini");
+
+            migrationBuilder.DropTable(
+                name: "Clienti");
+
+            migrationBuilder.DropTable(
+                name: "Prodotti");
+
+            migrationBuilder.DropTable(
+                name: "Categorie");
+        }
+    }
+}
